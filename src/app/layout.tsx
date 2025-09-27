23c7705d34f9ffc87eb36551de31cd76c31aab34
QuantumSnow0 <bmuthuri93@gmail.com>
2025-09-27 17:01:59 +0300

    fixed crawling issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,6 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", rel: "icon" },
-      { url: "/favicon.ico", rel: "shortcut icon" },
       {
         url: "/favicon-16x16.png",
         sizes: "16x16",
@@ -26,6 +24,8 @@ export const metadata: Metadata = {
         sizes: "32x32",
         type: "image/png",
       },
+      { url: "/favicon.ico", rel: "icon" },
+      { url: "/favicon.ico", rel: "shortcut icon" },
     ],
     apple: [
       {
@@ -57,6 +57,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="/favicon-16x16.png"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          sizes="180x180"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
